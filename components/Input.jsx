808cb76f601aafe380e4b7f5c0e81bd13da23b5a
lakhta2 <iamageneus@mail.ru>
2023-09-26 23:44:24 +0400

    add create-nft-page

---
+++ components/Input.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Input = ({ inputType, title, placeholder, handleClick }) => (
+  <div className="mt-10 w-full">
+    <p className="font-semibold font-poppins text-xl font-poppins dark:text-white text-nft-black-1">
+      {title}
+    </p>
+
+    { inputType === 'number' ? (
+      <div className="dark:bg-nft-black-1 bg-white border dark: border-nft-black-1 border-nft-gray-2 rounded-lg w-full outline-none font-poppins dark:text-white text-nft-gray-2 text-base mt-4 px-4 py-3 flexBetween flex-row">
+        <input
+          type="number"
+          className="flex w-full dark:bg-nft-black-1 bg-white outline-none"
+          placeholder={placeholder}
+          onChange={handleClick}
+        />
+        <p className="font-semibold font-poppins text-xl font-poppins dark:text-white text-nft-black-1">ETH</p>
+      </div>
+    ) : inputType === 'textarea' ? (
+      <textarea
+        rows={10}
+        className="dark:bg-nft-black-1 bg-white border dark: border-nft-black-1 border-nft-gray-2 rounded-lg w-full outline-none font-poppins dark:text-white text-nft-gray-2 text-base mt-4 px-4 py-3"
+        placeholder={placeholder}
+        onChange={handleClick}
+      />
+    ) : (
+      <input
+        className="dark:bg-nft-black-1 bg-white border dark: border-nft-black-1 border-nft-gray-2 rounded-lg w-full outline-none font-poppins dark:text-white text-nft-gray-2 text-base mt-4 px-4 py-3"
+        placeholder={placeholder}
+        onChange={handleClick}
+      />
+    )}
+  </div>
+);
+
+export default Input;
